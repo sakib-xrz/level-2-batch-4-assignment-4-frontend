12a27c2e8bb8a958c6fff52261557ab5189a7223
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2025-01-29 15:00:35 +0600

    feat: add Hero component to the home page and update layout

---
+++ src/pages/home/components/hero.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button } from "antd";
+
+const Hero: React.FC = () => {
+  return (
+    <section className="relative flex w-full items-center justify-center bg-[#18181b] sm:h-svh">
+      {/* Background Image */}
+      <div className="absolute inset-0">
+        <img
+          src="src/assets/images/hero.webp"
+          alt="Bicycle Store"
+          className="h-full w-full object-cover opacity-50"
+        />
+      </div>
+
+      {/* Content Box */}
+      <div className="bg-opacity-20 relative z-10 max-w-4xl rounded-xl px-4 py-8 text-center text-white backdrop-blur-lg sm:p-10">
+        <h1 className="text-4xl leading-tight font-extrabold sm:text-5xl md:text-6xl">
+          Elevate Your Ride <br />
+          <span className="text-[#c2ab8e]">Premium Bicycles</span>
+        </h1>
+        <p className="mt-4 text-sm text-gray-300 sm:text-base md:text-lg">
+          Discover cutting-edge bicycles engineered for speed, endurance, and
+          comfort. Designed for the ultimate riding experience. Shop now!
+        </p>
+        <div className="mt-6 flex justify-center gap-4">
+          <Button
+            type="primary"
+            className="rounded-lg border-none !bg-[#c2ab8e] px-6 py-3 text-lg text-black transition hover:!bg-[#a48d70]"
+          >
+            Shop Now
+          </Button>
+          <Button
+            type="default"
+            className="rounded-lg border-[#c2ab8e] px-6 py-3 text-lg text-[#c2ab8e] transition hover:!bg-[#c2ab8e] hover:!text-black"
+          >
+            Explore Bikes
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,9 @@
+import Hero from "./components/hero";
+
 export default function Home() {
   return (
-    <div className="flex h-[calc(100vh-300px)] items-center justify-center bg-gray-300">
-      Home
+    <div>
+      <Hero />
     </div>
   );
 }
